ffbdc531222eed55172a7979269e663cdf09b697
frlDev <99791510+frlDev@users.noreply.github.com>
2022-02-16 08:03:57 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+alert('Welcome')
+
+// DARK MODE 
+
+// const content = document.getElementsByTagName('body')[0];
+// const darkMode = document.getElementById('label');
+
+// darkMode.addEventListener('click', function() {
+//     darkMode.classList.toggle('active');
+//     content.classList.toggle('night');
+// })
+
+// Responsive Navbar
+
+const toggleNavbar = document.querySelector('.toggle-navbar');
+const navbar = document.querySelector('.navbar-links');
+
+toggleNavbar.addEventListener('click', function () {
+    alert('Showme the NAVBAR!')
+    navbar.classList.toggle('display-links');
+})
+
+
